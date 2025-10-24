d3c40858352925b3a90ec38ea289409dd2d39efd
maks2134 <maks210306@yandex.by>
2025-10-24 11:10:10 +0300

    create routes to auth module

--- cmd/main.go
+++ cmd/main.go
@@ -1,23 +1,25 @@
 package main
 
 import (
-	"fmt"
-	"mpb/configs"
-	"mpb/pkg/db"
+	"mpb/internal/auth"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/log"
 )
 
 func main() {
-	conf := configs.LoadConfig()
-	database := db.NewDb(conf)
+	//conf := configs.LoadConfig()
+	//database := db.NewDb(conf)
 
 	app := fiber.New(fiber.Config{
 		Prefork: true,
 	})
 
-	err := app.Listen(":3000")
+	api := app.Group("/api")
+	authRoutes := auth.NewAuthRoutes(api)
+	authRoutes.Register()
+
+	err := app.Listen(":8000")
 	if err != nil {
 		log.Fatal(err)
 	}
--- internal/auth/routes.go
+++ internal/auth/routes.go
@@ -1,1 +1,18 @@
 package auth
+
+import "github.com/gofiber/fiber/v2"
+
+type AuthRoutes struct {
+	router fiber.Router
+}
+
+func NewAuthRoutes(router fiber.Router) *AuthRoutes {
+	return &AuthRoutes{router: router}
+}
+
+func (r *AuthRoutes) Register() {
+	auth := r.router.Group("/auth")
+	auth.Post("/login", func(c *fiber.Ctx) error { return c.JSON(fiber.Map{"msg": "login ok"}) })
+	auth.Post("/register", func(c *fiber.Ctx) error { return c.JSON(fiber.Map{"msg": "register ok"}) })
+	auth.Post("/logout", func(c *fiber.Ctx) error { return c.JSON(fiber.Map{"msg": "logout ok"}) })
+}
