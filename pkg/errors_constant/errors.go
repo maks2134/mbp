ce32698963b8fea8e8899f1c0250715ad207a41e
maks2134 <maks210306@yandex.by>
2025-10-30 14:54:57 +0300

    create basic to service

--- pkg/errors_constant/errors.go
+++ pkg/errors_constant/errors.go
@@ -6,5 +6,6 @@ var (
 	UserAlreadyExists = errors.New("user already exists")
 	UserNotFound      = errors.New("user not found")
 	PostNotFound      = errors.New("post not found")
-	PostInvalidInput  = errors.New("invalid input")
+	InvalidTitle      = errors.New("title must be at least 3 characters long")
+	UserNotAuthorized = errors.New("user not authorized to modify this post")
 )
